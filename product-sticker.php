d85898170c37a31a31063612dd48aac547bbee13
Sergey Pronin <seregin-pro@ya.ru>
2024-02-02 12:42:55 +0300

    Add comment

--- product-sticker.php
+++ product-sticker.php
@@ -15,6 +15,8 @@ defined( 'ABSPATH' ) || exit;
 if (in_array ( 'woocommerce/woocommerce.php', apply_filters ( 'active_plugins', get_option ( 'active_plugins' ) ) ) ) {
 	
 	define ( 'SPS_VERSION', '1.0.0' );
+	
+	// New comment
 
 	if ( ! class_exists( 'ProductSticker', false ) ) {
 		include_once plugin_dir_path( __FILE__ ) . '/includes/class-product-sticker.php';
